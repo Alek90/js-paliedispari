027bab3ef81bfddb70f46e504e2201d552810428
Alek90 <4lek.90@gmail.com>
2021-10-19 15:20:34 +0200

    Scomposizione obbiettivo.
    Obbiettivo scomposto. Commentati strumenti da utilizzare. Avviata prima fase, testata in console risulta funzionante.

--- Palindromi/assets/main.js
+++ Palindromi/assets/main.js
@@ -3,3 +3,19 @@
     Creare una funzione per stabilire se la parola proposta sia palindroma o no.
 */
 
+
+// Chiedere all'utente di inserire una parola.
+// Strumenti: - prompt
+
+const user_word = prompt ("Scrivi una parola");
+console.log(user_word);
+ 
+
+// Creare una funzione per stabilire se la parola proposta sia palindroma o no.
+/* Strumenti: 
+    - function (); per creare la funzione;
+    - split(); per trasformare una stringa (user_word) in un array;
+    - reverse(); per invertire l'ordine degli elementi dell'array [user_word];
+    - join(); per convertire tutti gli elementi di un array in una stringa; ? (valutare se questo passaggio sia necessario)
+    - if(); per confrontare le due parole/i due array per verificare se siano uguali;
+*/
