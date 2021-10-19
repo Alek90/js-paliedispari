fce0035e5d56e71602870a581f731fc9cc322125
Alek90 <4lek.90@gmail.com>
2021-10-19 22:50:12 +0200

    Consegna esercizio.
    Riassegnati nomi classi e variabili con l'intenzione di favorire la lettura del cosdice.

--- Pari&Dispari/assets/main.js
+++ Pari&Dispari/assets/main.js
@@ -10,58 +10,65 @@
 // Funzioni
 
 /** Generatore numero random tra 1 e 5. */
-function randomNumber () {
+function randomNumberGenerator () {
 
     let random_number = Math.floor(Math.random() * 5) + 1;
 
     return random_number
-}
+};
 
 /** Funzione che riconosce e dichiara se un numero sia pari o dispari.
  * @param {number} numero - il numero da esaminare.
  */
-function pariDispari (numero) {
+function recognizerEvenOdd (numero) {
     const moduloPari = 0;
-    let result = "";
+    let declared = "";
 
     if (numero % 2 == moduloPari) {
-        result = "pari";
-        return result
+        declared = "pari";
+        return declared
     }else {
-        result = "dispari";
-        return result
-    }
+        declared = "dispari";
+        return declared
+    };
 }
 
 
 // Servirà qualcosa che dà il via? Magari un bottone da premere dopo aver selezionato le scelte...
 
-const play = document.getElementById("play");
+const playBotton = document.getElementById("play");
 
-// Azione
+// Azione: al click sul pulsante:
 
-play.addEventListener("click", function() {
+playBotton.addEventListener("click", function() {
 
+    // prendere la scelta dell'utente;
+    let user_choice = document.getElementById("user_choice").value;
+    // prendere il valore del numero scelto dall'utente, in forma di numero;
     const userNumberChoice = document.getElementById("user_number");
     const user_number = parseInt(userNumberChoice.value);
-    let user_choice = document.getElementById("user_choice").value;
+    // inizializzare variabile messaggio;
     let message = 0;
 
+    // generare numero PC tramite funzione apposita, e stamparlo in html;
+    let pc_number = randomNumberGenerator ();
+    document.getElementById("pc_number").innerHTML = pc_number;
 
-    let pc_number = randomNumber ();
-
+    // sommare numero user e numero PC;
     let sumNumber = user_number + pc_number;
 
-    sumResult = pariDispari(sumNumber);
-    console.log(user_number, user_choice, pc_number, sumNumber, sumResult);
+    // analizzare se la somma sia pari o dispari tramite funzione apposita;
+    sumDeclared = recognizerEvenOdd(sumNumber);
 
-    if(user_choice == sumResult){
+    // creare messaggi differenziati per i possibili risultati;
+    if(user_choice == sumDeclared){
         message = "You Win"
     }else {
         message = "PC Win"
     }
 
-    document.querySelector(".winner").innerHTML = message;
+    // stampare somma e messaggio reltivo al vincitore in html.
+    document.querySelector(".winner").innerHTML = `${sumNumber} ${message}`;
     console.log(message);
 
-})
+});
