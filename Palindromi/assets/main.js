bc086a2414f73e5653ca8bb0d006df7e2b3996f9
Alek90 <4lek.90@gmail.com>
2021-10-19 15:39:50 +0200

    Commenti pre esecuzione.
    Aggiunti commenti sulla base della documentazione individuata.

--- Palindromi/assets/main.js
+++ Palindromi/assets/main.js
@@ -14,8 +14,28 @@ console.log(user_word);
 // Creare una funzione per stabilire se la parola proposta sia palindroma o no.
 /* Strumenti: 
     - function (); per creare la funzione;
+*/
+
+// Modi trovati per costruire la funzione necessaria ad espletare lo scopo richiesto:
+
+/* 
     - split(); per trasformare una stringa (user_word) in un array;
     - reverse(); per invertire l'ordine degli elementi dell'array [user_word];
     - join(); per convertire tutti gli elementi di un array in una stringa; ? (valutare se questo passaggio sia necessario)
     - if(); per confrontare le due parole/i due array per verificare se siano uguali;
 */
+
+/* 
+    Oppure...utilizzando un ciclo for, e assegnando in maniera adeguata la variabile index, il termine del cicloed il codice da seguire...
+
+    let revers_word = "";
+    for (i = user_word.length - 1; i > 0; i--) {
+    reverse_word += user_word[i];
+    }
+    
+    Questo sulla base del fatto che la stringa viene interpretata come un array di caratteri.
+*/
+
+
+
+
