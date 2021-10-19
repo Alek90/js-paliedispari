0fbe6e357b3139b2ad624691aa10f0ee081d9356
Alek90 <4lek.90@gmail.com>
2021-10-19 18:19:31 +0200

    Creata funzione pari o dispari.
    Creata funzione volta a stabilire se la somma del numero utente e del numero random sia pari o dispari.

--- Pari&Dispari/assets/main.js
+++ Pari&Dispari/assets/main.js
@@ -19,3 +19,25 @@ function randomNumber () {
 
 let pc_number = randomNumber ()
 console.log(pc_number);
+
+
+// Funzione basata sulla concezione che num pari + num dispari darà risultato dispari, mentre la somma tra 2 num pari o 2 num dispari darà risultato pari.
+function pariDispari (x, y) {
+    let modulo1 = x % 2;
+    let modulo2 = y % 2;
+    let result = "";
+
+    if(modulo1 |= modulo2){
+        result = "Dispari";
+        return result;
+    }else{
+        result = "Pari";
+        return result;
+    };
+}
+
+const x = 4
+const y = 5
+
+let risultato = pariDispari(x, pc_number)
+console.log(risultato);
