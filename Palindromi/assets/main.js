274aff5c4ce081f1be1d923b43237c511c57b945
Alek90 <4lek.90@gmail.com>
2021-10-19 14:38:58 +0200

    Organizzato lavoro.
    Creata repo e cartelle relative ai 2 esercizi. Per ogni cartella/esercizio creati file html e js, collegati, e commentati obbiettivi.

---
+++ Palindromi/assets/main.js
@@ -0,0 +1,5 @@
+/* OBBIETTIVO:
+    Chiedere all'utente di inserire una parola.
+    Creare una funzione per stabilire se la parola proposta sia palindroma o no.
+*/
+
---
+++ Pari&Dispari/assets/main.js
@@ -0,0 +1,8 @@
+/*   OBBIETTIVI:
+    Dare la possibilità all'utente di scegliere tra prari e dispari.
+    Dare la possibilità all'utente di scegliere un numero compreso tra 1 e 5.
+    Generare un numero random compreso tra 1 e 5 per il computer, per mezzo di una funzione.
+    Sommiamo il numero dell'utente e quello generato per il pc.
+    Stabilire se la somma sia pari o dispari, tramite una funzione.
+    Dichiarare il vincitore.
+*/
